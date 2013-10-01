0c557d935d68232131ebbe130deacab9211dc91b
leonardaustin <leonardaustin@gmail.com>
2013-10-01 20:16:40 +0100

    Stuff (including netbeans cruft)

---
+++ MyFirstApp/src/myfirstapp/MyFirstApp.java
@@ -0,0 +1,22 @@
+package myfirstapp;
+
+/**
+ *
+ * @author leonard
+ */
+public class MyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double first_number, second_number, answer;
+        
+        first_number = 10.5;
+        second_number = 20.8;
+        answer = first_number + second_number;
+        
+        System.out.println("My First Print Line" + answer);
+    }
+}
